8756fa41a064ad8e26075513e41ee081d93a35b8
Cameron Blandford <cam@cos.io>
2018-03-27 21:35:37 -0400

    Add example validation

--- app/components/workflow-basics.js
+++ app/components/workflow-basics.js
@@ -1,4 +1,18 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  validDOI: true,
+  validTitle: true,
+  actions: {
+    validateDOI() {
+      const doi = this.get('model.doi');
+      const newDOIRegExp = /^10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
+      const ancientDOIRegExp = /^10.1002\/[^\s]+$/i;
+      this.set('validDOI', doi == null || newDOIRegExp.test(doi) || ancientDOIRegExp.test(doi));
+    },
+    validateTitle() {
+      const title = this.get('model.title');
+      this.set('validTitle', title == null || title.length > 5);
+    }
+  }
 });
